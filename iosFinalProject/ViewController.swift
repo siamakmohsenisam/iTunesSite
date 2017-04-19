23b9fec738a09ba38d26a91fd49e2dcfe1e16444
Siamak Sam <siamakmohsenisam@Siamaks-MacBook-Pro.local>
2017-04-19 10:17:50 -0400

    Initial Commit

---
+++ iosFinalProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iosFinalProject
+//
+//  Created by Siamak Mohseni Sam on 2017-04-19.
+//  Copyright © 2017 Siamak Mohseni Sam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
